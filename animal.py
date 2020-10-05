7f63ab6d870e0f491c4927fc96a154032108cddb
jamarrhill <hilljama@oregonstate.edu>
2020-10-04 22:10:09 -0800

    animal.py

---
+++ animal.py
@@ -0,0 +1,24 @@
+# Author: Jamar Hill
+# Date: 10/4/2020
+
+print("Hi! What is your favorite animal?")
+fave_animal = input("Enter your favorite animal here:")
+print("Your favorite animal is the " + fave_animal + ".")
+
+
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
